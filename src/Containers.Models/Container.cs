5980fd550f37d9c9325748c751c185db01badfe2
turaljabbarli <s31522@pjwstk.edu.pl>
2025-04-18 16:23:57 +0200

    initial commit

--- src/Containers.Models/Container.cs
+++ src/Containers.Models/Container.cs
@@ -2,5 +2,13 @@
 
 public class Container
 {
+
+    public int ID { get; set; }
+    
+    public int ContainerTypeId { get; set; }
+    
+    public bool IsHazzarious { get; set; }
+    
+    public string Name { get; set; }
     
 }
--- src/Containers.Models/ContainerType.cs
+++ src/Containers.Models/ContainerType.cs
@@ -3,4 +3,9 @@
 public class ContainerType
 {
     
+    public int ID { get; set; }
+    
+    public string Name { get; set; }
+
+    
 }
